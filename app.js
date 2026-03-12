5518577da800e2cf8c1434be616a63ed7082717b
prajapathimanshu621-oss <prajapathimanshu621@gmail.com>
2026-03-12 11:45:32 +0530

    Refactor cart functionality and update display logic

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-const cakes=[
-
-{name:"Chocolate Cake",price:240},
-{name:"Black Forest",price:200},
-{name:"Strawberry Cake",price:200},
-{name:"Vanilla Cake",price:200},
-{name:"Pineapple Cake",price:200}
-
-]
-
 let cart=[]
 
-function loadCakes(){
+function addCart(i){
+
+cart.push(cakes[i])
+
+showCart()
+
+}
+
+function showCart(){
 
 let html=""
 
-cakes.forEach((cake,i)=>{
+cart.forEach((c,i)=>{
 
 html+=`
 
-<div class="cake">
+<div>
 
-<h3>${cake.name}</h3>
+${c.name} ₹${c.price}
 
-₹${cake.price}
+<button onclick="removeCart(${i})">
 
-<br>
-
-<button onclick="addCart(${i})">
-
-Add to Cart
+Delete
 
 </button>
 
@@ -38,75 +32,14 @@ Add to Cart
 
 })
 
-document.getElementById("cakeList").innerHTML=html
+document.getElementById("cart").innerHTML=html
 
 }
 
-loadCakes()
+function removeCart(i){
 
-function addCart(i){
+cart.splice(i,1)
 
-cart.push(cakes[i])
+showCart()
 
-updateCart()
-
-}
-
-function updateCart(){
-
-let html=""
-
-cart.forEach(c=>{
-
-html+=c.name+" ₹"+c.price+"<br>"
-
-})
-
-document.getElementById("cartItems").innerHTML=html
-
-}
-
-function checkout(){
-
-navigator.geolocation.getCurrentPosition(function(position){
-
-let lat=position.coords.latitude
-let lng=position.coords.longitude
-
-let shopLat=26.8818326
-let shopLng=75.2389793
-
-let distance=getDistance(lat,lng,shopLat,shopLng)
-
-if(distance<=50){
-
-alert("Delivery Available")
-
-}else{
-
-alert("Delivery not available")
-
-}
-
-})
-
-}
-
-function getDistance(lat1,lon1,lat2,lon2){
-
-const R=6371
-
-const dLat=(lat2-lat1)*Math.PI/180
-const dLon=(lon2-lon1)*Math.PI/180
-
-const a=
-Math.sin(dLat/2)*Math.sin(dLat/2)+
-Math.cos(lat1*Math.PI/180)*
-Math.cos(lat2*Math.PI/180)*
-Math.sin(dLon/2)*Math.sin(dLon/2)
-
-const c=2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a))
-
-return R*c
-
-}
+}}
